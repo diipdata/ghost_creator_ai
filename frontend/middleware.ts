ee557333dbd1dd49e06ef9e8bfa0d648a02add8f
diipdata <diegop.freitas@gmail.com>
2025-05-01 01:33:56 -0300

    ajustes de authMiddleware para clerkMiddleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,14 +1,23 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-// Definir rotas públicas
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)", "/obrigado"]);
+// Define as rotas públicas que não exigem autenticação
+const isPublicRoute = createRouteMatcher([ 
+  "/", 
+  "/sign-in(.*)", 
+  "/sign-up(.*)", 
+  "/obrigado"
+]);
 
-export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
-    auth.protect(); // correto: sem "()"
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) { await auth.protect();
   }
 });
 
 export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)"],
+  matcher: [
+    // Ignora arquivos estáticos e internos do Next.js
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Sempre aplica middleware em rotas de API
+    '/(api|trpc)(.*)',
+  ],
 };
